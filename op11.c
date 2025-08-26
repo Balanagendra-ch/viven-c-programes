e0a42edb93befd2131382b0a020d82e7db8e62c2
Balanagendra-ch <chbalanagendra955@gmail.com>
2025-08-26 14:49:36 +0530

    Add files via upload

---
+++ op11.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+printf("value is =%d",(10++));
+}
---
+++ op3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MOBILE 0x01
+#define LAPPY 0x02
+int main()
+{
+unsigned char item=0x00;
+item |=MOBILE;
+item |=LAPPY;
+printf("i have purchased....");
+if(item & MOBILE){
+printf("Mobile, ");
+}
+if(item & LAPPY){
+printf("Lappy");
+}
+return 1;
+}

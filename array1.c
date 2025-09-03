9910a40a84a7157938c36dd5aa3db39a268e0b31
Balanagendra-ch <chbalanagendra955@gmail.com>
2025-09-03 11:31:51 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int arr[100],n,i;
+printf("enter number of elements(max 100):  ");
+scanf("%d",&n);
+printf("enter %d elements:\n",n);
+for (i = 0;i<n; i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("the element in the array are:\n");
+for(i=0;i<n; i++)
+{
+printf("%d",arr[i]);
+}
+printf("\n");
+return 0;
+}
